0ee7592b366361072e3086695f772ae83aec037f
Nam Do <ujhqw@student.kit.edu>
2025-09-29 16:55:13 +0200

    more core count benchmarks

---
+++ benchmarks_hits/core_count_benchmark/iteration1/core40/main.py
@@ -0,0 +1,7 @@
+from raxtax_extension_prototype.simulator import run_simulation
+from raxtax_extension_prototype.config_handler import create_config_here
+
+if __name__ == "__main__":
+    create_config_here(redo_config=False, leaf_count=1000, sequence_length=50000, tree_height=0.1, query_count=200, core_count=40,
+                       query_min_lenght=100, fragment_count=50)
+    run_simulation()
