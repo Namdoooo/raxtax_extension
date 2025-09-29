1610959d5c6a1a4e6c95b0d367eb74a9d60e4789
Nam Do <ujhqw@student.kit.edu>
2025-09-29 02:33:05 +0200

    add core_count_benchmark iteration1

--- benchmarks_hits/core_count_benchmark/iteration1/core8/main.py
+++ benchmarks_hits/core_count_benchmark/iteration1/core8/main.py
@@ -2,6 +2,6 @@ from raxtax_extension_prototype.simulator import run_simulation
 from raxtax_extension_prototype.config_handler import create_config_here
 
 if __name__ == "__main__":
-    create_config_here(redo_config=False, leaf_count=1000, sequence_length=50000, tree_height=0.1, query_count=200, core_count=0,
+    create_config_here(redo_config=False, leaf_count=1000, sequence_length=50000, tree_height=0.1, query_count=200, core_count=8,
                        query_min_lenght=100, fragment_count=50)
     run_simulation()
---
+++ benchmarks_hits/core_count_benchmark/iteration1/main.py
@@ -0,0 +1,4 @@
+from raxtax_extension_prototype.simulator import run_all_main
+
+if __name__ == "__main__":
+    run_all_main()
