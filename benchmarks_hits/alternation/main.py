daa97cdda2e094564233d27a4cb0500bc9d1d57e
Nam Do <ujhqw@student.kit.edu>
2025-10-24 13:22:17 +0200

    alternation benchmark

--- benchmarks_hits/alternation/main.py
+++ benchmarks_hits/alternation/main.py
@@ -5,7 +5,7 @@ from simtools.simulator import run_main_list
 if __name__ == "__main__":
     main_list = []
     base_dir = Path(__file__).resolve().relative_to(Path.cwd()).parent
-    for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    for i in range(11, 31):
         main_path = base_dir / f"iteration{i}"
         main_list.append(main_path)
         print(main_path)
