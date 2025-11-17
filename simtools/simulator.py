f8dcbdb8025b9d461d112d1b5e1141e93b4c971e
Nam Do <ujhqw@student.kit.edu>
2025-11-18 00:16:53 +0100

    remove usr/bin/time functionaliy as raised errors in benchmarks

--- simtools/simulator.py
+++ simtools/simulator.py
@@ -123,7 +123,8 @@ def run_main_list(main_dir_list: list[Path]):
             time_log_path = "/".join(rel_parts[:-1]) + "/time.log"
             print(time_log_path)
             print(f"Running {module_path}...")
-            subprocess.run(["/usr/bin/time", "-v", "-o", time_log_path,"python", "-m", module_path], check=True)
+            #subprocess.run(["/usr/bin/time", "-v", "-o", time_log_path,"python", "-m", module_path], check=True)
+            subprocess.run(["python", "-m", module_path], check=True)
 
 if __name__ == "__main__":
     pass
