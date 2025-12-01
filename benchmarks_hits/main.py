2f1119a2c755feb3cd0f245653104ce4d50e6aeb
Nam Do <ujhqw@student.kit.edu>
2025-12-01 03:20:10 +0100

    fix mistake

--- benchmarks_hits/main.py
+++ benchmarks_hits/main.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     main_list = []
     base_dir = Path(__file__).resolve().relative_to(Path.cwd()).parent
 
-    query_memory_benchmark_new_dir = base_dir / "query_memory_benchmark_new"
-    reference_memory_benchmark_new_dir = base_dir / "reference_memory_benchmark_new"
+    query_memory_benchmark_new_dir = base_dir / "query_memory_benchmark_new" / "iteration1"
+    reference_memory_benchmark_new_dir = base_dir / "reference_memory_benchmark_new" / "iteration1"
     main_list.append(query_memory_benchmark_new_dir)
     main_list.append(reference_memory_benchmark_new_dir)
 
