3c329983d9cd8ec1f50963794f59d75719afc8f9
Nam Do <ujhqw@student.kit.edu>
2025-12-16 17:39:22 +0100

    reference_count_benchmark archive ready

--- benchmarks_hits/reference_count_benchmark/iteration2/main.py
+++ benchmarks_hits/reference_count_benchmark/iteration2/main.py
@@ -61,5 +61,5 @@ if __name__ == '__main__':
         create_main_at_path(base_dir=main_dir, redo_main=False)
         main_dir_list.append(main_dir)
 
-    #run_main_list(main_dir_list)
+    run_main_list(main_dir_list)
 
